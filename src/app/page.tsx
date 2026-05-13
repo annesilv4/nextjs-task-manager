f48026a211bac88a82b7f61c52728c2902a58907
annesilv4 <carolaynebarbozasilva@gmail.com>
2026-05-13 20:43:40 -0300

    implementa a página inicial com a lista de tarefas, incluindo componentes Title e Grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
-export default function Home() {
+import { getTarefas } from "@/data/tarefas";
+import Title from "./components/Title";
+import Grid from "./components/Grid";
+
+export default async function Home() {
+  const tasks = await getTarefas();
+
   return (
     <>
+      <Title title="Lista de Tarefas" />
+      <Grid tarefas={tasks} />
     </>
   );
 }
