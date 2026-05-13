a7f6392f2147cee3bb68a99fba8fa68593827971
annesilv4 <carolaynebarbozasilva@gmail.com>
2026-05-12 21:15:23 -0300

    atualiza o componente Grid para exibir o total de tarefas e ajusta o layout

--- src/app/components/Grid/index.tsx
+++ src/app/components/Grid/index.tsx
@@ -5,6 +5,7 @@ import Style from "./Grid.module.css";
 import Card from "../Card";
 import DefaultButton from "../DefaultButton";
 import { useRouter } from "next/navigation";
+import useContadorDeTarefas from "@/hooks/useContadorDeTarefas";
 
 type GridProps = {
     tarefas: Tarefa[];
@@ -12,10 +13,13 @@ type GridProps = {
 
 const Grid = ({ tarefas }: GridProps) => {
     const router = useRouter();
+    const totalTarefas = useContadorDeTarefas(tarefas);
 
     return (
         <>
-            <div className={Style.btnAdd}>
+
+            <div className={Style.info}>
+                <h2 className={Style.total}>Você possuí {totalTarefas} tarefas</h2>
                 <DefaultButton onClick={() => router.push("/new-task")}>Adicionar Tarefa</DefaultButton>
             </div>
             <section className={Style.grid}>
