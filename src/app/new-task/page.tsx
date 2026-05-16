cc815f4231a361b362d8781c1090ab56de432def
annesilv4 <carolaynebarbozasilva@gmail.com>
2026-05-16 13:02:47 -0300

    adiciona a página NewTask com componentes Title e NovaTarefa para criação de novas tarefas

---
+++ src/app/new-task/page.tsx
@@ -0,0 +1,11 @@
+import Title from "../components/Title";
+import NovaTarefa from "../components/NovaTarefa";
+
+export default function NewTask() {
+    return (
+        <>
+            <Title title="Nova Tarefa" />
+            <NovaTarefa />
+        </>
+    )
+}
